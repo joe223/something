eab7213340a21e9d1efc494f42d9ae3ef96b4cbd
joe223 <heyin223@163.com>
2017-09-09 21:28:40 +0800

    then function trigger the task

--- promise.js
+++ promise.js
@@ -1,45 +1,4 @@
-// // Promise.all
-
-// function all (list) {
-//     const taskList = new Array(list.length);
-//     let count = list.length;        // in case res === undefined
-    
-//     return new Promise((resolve, reject) => {
-//         list.map((item, index, arr) => {
-//             list[index].then(res => {
-//                 taskList[index] = res;
-//                 (--count === 0) && resolve(taskList);
-//             }).catch(err => {
-//                 reject(err);
-//             });
-//         });
-//     });
-// }
-
-// // test code
-// let list = [
-//     new Promise((resolve, reject) => {
-//         setTimeout(res => {
-//             resolve()
-//         }, 1000);
-//     }), 
-//     new Promise((resolve, reject) => {
-//         setTimeout(res => {
-//             resolve('2 done')
-//         }, 2000);
-//     }),
-//     new Promise((resolve, reject) => {
-//         setTimeout(res => {
-//             reject('3 error')
-//         }, 2000);
-//     }),
-// ];
-
-// all(list).then(res => {
-//     console.log('all result:', res);
-// }).catch(err => {
-//     console.error('err:', err);
-// })
+// Promise
 
 
 function Promise (task) {
@@ -51,7 +10,7 @@ function Promise (task) {
 Promise.prototype.then = function (resolve) {
     this.resolve = resolve || function () {};
     this.task && this.task(this.resolve, this.reject);    
-    console.log(this.resolve, this.reject);    
+
     return this;
 }
 Promise.prototype.catch = function (reject) {
@@ -59,13 +18,58 @@ Promise.prototype.catch = function (reject) {
     return this;
 }
 
-let a = new Promise((resolve, reject) => {
-    setTimeout(e => {
-        resolve('resolve')
-    }, 0)
-}).then((e) => {
-    console.log(e);
+
+// Promise.all
+
+function all (list) {
+    const taskList = new Array(list.length);
+    let count = list.length;        // in case res === undefined
+    
+    return new Promise((resolve, reject) => {
+        list.map((item, index, arr) => {
+            list[index].then(res => {
+                taskList[index] = res;
+                (--count === 0) && resolve(taskList);
+            }).catch(err => {
+                reject(err);
+            });
+        });
+    });
+}
+
+// test code
+let list = [
+    new Promise((resolve, reject) => {
+        setTimeout(res => {
+            resolve()
+        }, 1000);
+    }), 
+    new Promise((resolve, reject) => {
+        setTimeout(res => {
+            resolve('2 done')
+        }, 2000);
+    }),
+    new Promise((resolve, reject) => {
+        setTimeout(res => {
+            resolve('3 error')          // reject cause error
+        }, 2000);
+    }),
+];
+
+all(list).then(res => {
+    console.log('all result:', res);
 }).catch(err => {
-    console.error(err);
-});
+    console.error('err:', err);
+})
+
+
+// let a = new Promise((resolve, reject) => {
+//     setTimeout(e => {
+//         resolve('resolve')
+//     }, 0)
+// }).then((e) => {
+//     console.log(e);
+// }).catch(err => {
+//     console.error(err);
+// });
 
